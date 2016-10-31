a9ff4a856f5b69ac208d1a663b14cc51842663f3
Si13n7 <si13n7@live.de>
2016-10-31 17:44:34 +0100

    Prevent possible non-nullable type exceptions.

--- src/SilDev/QuickWmi/Win32_OperatingSystem.cs
+++ src/SilDev/QuickWmi/Win32_OperatingSystem.cs
@@ -5,7 +5,7 @@
 // ==============================================
 // 
 // Filename: Win32_OperatingSystem.cs
-// Version:  2016-10-24 16:06
+// Version:  2016-10-31 17:36
 // 
 // Copyright (c) 2016, Si13n7 Developments (r)
 // All rights reserved.
@@ -36,62 +36,62 @@ namespace SilDev.QuickWmi
         public static string CSCreationClassName => GetValue(nameof(CSCreationClassName));
         public static string CSDVersion => GetValue(nameof(CSDVersion));
         public static string CSName => GetValue(nameof(CSName));
-        public static short CurrentTimeZone => GetValue(nameof(CurrentTimeZone));
-        public static bool DataExecutionPrevention_Available => GetValue(nameof(DataExecutionPrevention_Available));
-        public static bool DataExecutionPrevention_32BitApplications => GetValue(nameof(DataExecutionPrevention_32BitApplications));
-        public static bool DataExecutionPrevention_Drivers => GetValue(nameof(DataExecutionPrevention_Drivers));
-        public static byte DataExecutionPrevention_SupportPolicy => GetValue(nameof(DataExecutionPrevention_SupportPolicy));
-        public static bool Debug => GetValue(nameof(Debug));
+        public static short? CurrentTimeZone => GetValue(nameof(CurrentTimeZone));
+        public static bool? DataExecutionPrevention_Available => GetValue(nameof(DataExecutionPrevention_Available));
+        public static bool? DataExecutionPrevention_32BitApplications => GetValue(nameof(DataExecutionPrevention_32BitApplications));
+        public static bool? DataExecutionPrevention_Drivers => GetValue(nameof(DataExecutionPrevention_Drivers));
+        public static byte? DataExecutionPrevention_SupportPolicy => GetValue(nameof(DataExecutionPrevention_SupportPolicy));
+        public static bool? Debug => GetValue(nameof(Debug));
         public static string Description => GetValue(nameof(Description));
-        public static bool Distributed => GetValue(nameof(Distributed));
-        public static uint EncryptionLevel => GetValue(nameof(EncryptionLevel));
-        public static byte ForegroundApplicationBoost => GetValue(nameof(ForegroundApplicationBoost));
-        public static ulong FreePhysicalMemory => GetValue(nameof(FreePhysicalMemory));
-        public static ulong FreeSpaceInPagingFiles => GetValue(nameof(FreeSpaceInPagingFiles));
-        public static ulong FreeVirtualMemory => GetValue(nameof(FreeVirtualMemory));
-        public static DateTime InstallDate => GetValue(nameof(InstallDate), typeof(DateTime));
-        public static uint LargeSystemCache => GetValue(nameof(LargeSystemCache));
-        public static DateTime LastBootUpTime => GetValue(nameof(LastBootUpTime), typeof(DateTime));
-        public static DateTime LocalDateTime => GetValue(nameof(LocalDateTime), typeof(DateTime));
+        public static bool? Distributed => GetValue(nameof(Distributed));
+        public static uint? EncryptionLevel => GetValue(nameof(EncryptionLevel));
+        public static byte? ForegroundApplicationBoost => GetValue(nameof(ForegroundApplicationBoost));
+        public static ulong? FreePhysicalMemory => GetValue(nameof(FreePhysicalMemory));
+        public static ulong? FreeSpaceInPagingFiles => GetValue(nameof(FreeSpaceInPagingFiles));
+        public static ulong? FreeVirtualMemory => GetValue(nameof(FreeVirtualMemory));
+        public static DateTime? InstallDate => GetValue(nameof(InstallDate), typeof(DateTime));
+        public static uint? LargeSystemCache => GetValue(nameof(LargeSystemCache));
+        public static DateTime? LastBootUpTime => GetValue(nameof(LastBootUpTime), typeof(DateTime));
+        public static DateTime? LocalDateTime => GetValue(nameof(LocalDateTime), typeof(DateTime));
         public static string Locale => GetValue(nameof(Locale));
         public static string Manufacturer => GetValue(nameof(Manufacturer));
-        public static uint MaxNumberOfProcesses => GetValue(nameof(MaxNumberOfProcesses));
-        public static ulong MaxProcessMemorySize => GetValue(nameof(MaxProcessMemorySize));
+        public static uint? MaxNumberOfProcesses => GetValue(nameof(MaxNumberOfProcesses));
+        public static ulong? MaxProcessMemorySize => GetValue(nameof(MaxProcessMemorySize));
         public static string[] MUILanguages => GetValue(nameof(MUILanguages));
         public static string Name => GetValue(nameof(Name));
-        public static uint NumberOfLicensedUsers => GetValue(nameof(NumberOfLicensedUsers));
-        public static uint NumberOfProcesses => GetValue(nameof(NumberOfProcesses));
-        public static uint NumberOfUsers => GetValue(nameof(NumberOfUsers));
-        public static uint OperatingSystemSKU => GetValue(nameof(OperatingSystemSKU));
+        public static uint? NumberOfLicensedUsers => GetValue(nameof(NumberOfLicensedUsers));
+        public static uint? NumberOfProcesses => GetValue(nameof(NumberOfProcesses));
+        public static uint? NumberOfUsers => GetValue(nameof(NumberOfUsers));
+        public static uint? OperatingSystemSKU => GetValue(nameof(OperatingSystemSKU));
         public static string Organization => GetValue(nameof(Organization));
         public static string OSArchitecture => GetValue(nameof(OSArchitecture));
-        public static uint OSLanguage => GetValue(nameof(OSLanguage));
-        public static uint OSProductSuite => GetValue(nameof(OSProductSuite));
-        public static ushort OSType => GetValue(nameof(OSType));
+        public static uint? OSLanguage => GetValue(nameof(OSLanguage));
+        public static uint? OSProductSuite => GetValue(nameof(OSProductSuite));
+        public static ushort? OSType => GetValue(nameof(OSType));
         public static string OtherTypeDescription => GetValue(nameof(OtherTypeDescription));
-        public static bool PAEEnabled => GetValue(nameof(PAEEnabled));
+        public static bool? PAEEnabled => GetValue(nameof(PAEEnabled));
         public static string PlusProductID => GetValue(nameof(PlusProductID));
         public static string PlusVersionNumber => GetValue(nameof(PlusVersionNumber));
-        public static bool PortableOperatingSystem => GetValue(nameof(PortableOperatingSystem));
-        public static bool Primary => GetValue(nameof(Primary));
-        public static uint ProductType => GetValue(nameof(ProductType));
+        public static bool? PortableOperatingSystem => GetValue(nameof(PortableOperatingSystem));
+        public static bool? Primary => GetValue(nameof(Primary));
+        public static uint? ProductType => GetValue(nameof(ProductType));
         public static string RegisteredUser => GetValue(nameof(RegisteredUser));
         public static string SerialNumber => GetValue(nameof(SerialNumber));
-        public static ushort ServicePackMajorVersion => GetValue(nameof(ServicePackMajorVersion));
-        public static ushort ServicePackMinorVersion => GetValue(nameof(ServicePackMinorVersion));
-        public static ulong SizeStoredInPagingFiles => GetValue(nameof(SizeStoredInPagingFiles));
+        public static ushort? ServicePackMajorVersion => GetValue(nameof(ServicePackMajorVersion));
+        public static ushort? ServicePackMinorVersion => GetValue(nameof(ServicePackMinorVersion));
+        public static ulong? SizeStoredInPagingFiles => GetValue(nameof(SizeStoredInPagingFiles));
         public static string Status => GetValue(nameof(Status));
-        public static uint SuiteMask => GetValue(nameof(SuiteMask));
+        public static uint? SuiteMask => GetValue(nameof(SuiteMask));
         public static string SystemDevice => GetValue(nameof(SystemDevice));
         public static string SystemDirectory => GetValue(nameof(SystemDirectory));
         public static string SystemDrive => GetValue(nameof(SystemDrive));
-        public static ulong TotalSwapSpaceSize => GetValue(nameof(TotalSwapSpaceSize));
-        public static ulong TotalVirtualMemorySize => GetValue(nameof(TotalVirtualMemorySize));
-        public static ulong TotalVisibleMemorySize => GetValue(nameof(TotalVisibleMemorySize));
+        public static ulong? TotalSwapSpaceSize => GetValue(nameof(TotalSwapSpaceSize));
+        public static ulong? TotalVirtualMemorySize => GetValue(nameof(TotalVirtualMemorySize));
+        public static ulong? TotalVisibleMemorySize => GetValue(nameof(TotalVisibleMemorySize));
         public static Version Version => GetValue(nameof(Version), typeof(Version));
         public static string WindowsDirectory => GetValue(nameof(WindowsDirectory));
-        public static byte QuantumLength => GetValue(nameof(QuantumLength));
-        public static byte QuantumType => GetValue(nameof(QuantumType));
+        public static byte? QuantumLength => GetValue(nameof(QuantumLength));
+        public static byte? QuantumType => GetValue(nameof(QuantumType));
 
         private static dynamic GetValue(string name, Type type)
         {
