f566b705fdda113e9b34f3eaeb17f545fd5a811d
Si13n7 <si13n7@live.de>
2016-11-25 04:27:57 +0100

    Expand the functionality of the ContextMenuStripEx class.

--- src/SilDev/Forms/ContextMenuStripEx.cs
+++ src/SilDev/Forms/ContextMenuStripEx.cs
@@ -5,7 +5,7 @@
 // ==============================================
 // 
 // Filename: ContextMenuStripEx.cs
-// Version:  2016-10-27 13:53
+// Version:  2016-11-25 04:25
 // 
 // Copyright (c) 2016, Si13n7 Developments (r)
 // All rights reserved.
@@ -16,6 +16,7 @@
 namespace SilDev.Forms
 {
     using System;
+    using System.Collections.Generic;
     using System.Drawing;
     using System.Drawing.Drawing2D;
     using System.Windows.Forms;
@@ -25,6 +26,131 @@ namespace SilDev.Forms
     /// </summary>
     public static class ContextMenuStripEx
     {
+        /// <summary>
+        ///     Provides enumerated values of <see cref="ContextMenuStrip"/> animations.
+        /// </summary>
+        public enum Animations : uint
+        {
+            /// <summary>
+            ///     Smooth fade in animation.
+            /// </summary>
+            Default = 0x0,
+
+            /// <summary>
+            ///     Fade in animation.
+            /// </summary>
+            Blend = WinApi.AnimateWindowFlags.AW_BLEND,
+
+            /// <summary>
+            ///     Makes the <see cref="ContextMenuStrip"/> appear to collapse inward.
+            /// </summary>
+            Center = WinApi.AnimateWindowFlags.AW_CENTER,
+
+            /// <summary>
+            ///     Slide animation from left to right.
+            /// </summary>
+            SlideHorPositive = WinApi.AnimateWindowFlags.AW_SLIDE | WinApi.AnimateWindowFlags.AW_HOR_POSITIVE,
+
+            /// <summary>
+            ///     Slide animation from right to left.
+            /// </summary>
+            SlideHorNegative = WinApi.AnimateWindowFlags.AW_SLIDE | WinApi.AnimateWindowFlags.AW_HOR_NEGATIVE,
+
+            /// <summary>
+            ///     Slide animation from top to bottom.
+            /// </summary>
+            SlideVerPositive = WinApi.AnimateWindowFlags.AW_SLIDE | WinApi.AnimateWindowFlags.AW_VER_POSITIVE,
+
+            /// <summary>
+            ///     Slide animation from bottom to top.
+            /// </summary>
+            SlideVerNegative = WinApi.AnimateWindowFlags.AW_SLIDE | WinApi.AnimateWindowFlags.AW_VER_NEGATIVE
+        }
+
+        private static readonly Dictionary<ContextMenuStrip, object[]> EnabledAnimation = new Dictionary<ContextMenuStrip, object[]>();
+
+        /// <summary>
+        ///     Closes this <see cref="ContextMenuStrip"/> when the mouse cursor leaves it.
+        /// </summary>
+        /// <param name="contextMenuStrip">
+        ///     The <see cref="ContextMenuStrip"/>.
+        /// </param>
+        /// <param name="toleration">
+        ///     The toleration in pixel.
+        /// </param>
+        public static void CloseOnMouseLeave(this ContextMenuStrip contextMenuStrip, int toleration = 0)
+        {
+            if (contextMenuStrip == null)
+                return;
+            if (toleration < 0)
+                toleration = 0;
+            contextMenuStrip.Opened += (sender, args) =>
+            {
+                var timer = new Timer
+                {
+                    Interval = 1,
+                    Enabled = true
+                };
+                timer.Tick += (s, a) =>
+                {
+                    var rect = contextMenuStrip.ClientRectangle;
+                    rect = new Rectangle(rect.X - toleration, rect.Y - toleration, rect.Width + toleration * 2, rect.Height + toleration * 2);
+                    if (rect.Contains(contextMenuStrip.PointToClient(Control.MousePosition)))
+                        return;
+                    contextMenuStrip.Close();
+                    timer.Dispose();
+                };
+            };
+        }
+
+        /// <summary>
+        ///     Enables you to produce special effects when showing this <see cref="ContextMenuStrip"/>.
+        /// </summary>
+        /// <param name="contextMenuStrip">
+        ///     The <see cref="ContextMenuStrip"/>.
+        /// </param>
+        /// <param name="animation">
+        ///     The type of animation.
+        /// </param>
+        /// <param name="time">
+        ///     The time it takes to play the animation, in milliseconds.
+        /// </param>
+        public static void EnableAnimation(this ContextMenuStrip contextMenuStrip, Animations animation = Animations.Default, int time = 200)
+        {
+            if (contextMenuStrip == null)
+                return;
+            var aniConfig = new object[] { time, animation };
+            if (EnabledAnimation.ContainsKey(contextMenuStrip))
+            {
+                EnabledAnimation[contextMenuStrip] = aniConfig;
+                return;
+            }
+            EnabledAnimation.Add(contextMenuStrip, aniConfig);
+            contextMenuStrip.Opening += (sender, args) =>
+            {
+                if (animation != Animations.Default)
+                {
+                    WinApi.UnsafeNativeMethods.AnimateWindow(contextMenuStrip.Handle, (int)EnabledAnimation[contextMenuStrip][0], (WinApi.AnimateWindowFlags)EnabledAnimation[contextMenuStrip][1]);
+                    return;
+                }
+                contextMenuStrip.Opacity = 0d;
+                var timer = new Timer
+                {
+                    Interval = 1,
+                    Enabled = true
+                };
+                timer.Tick += (s, a) =>
+                {
+                    if (contextMenuStrip.Opacity < 1d)
+                    {
+                        contextMenuStrip.Opacity += .1d;
+                        return;
+                    }
+                    timer.Dispose();
+                };
+            };
+        }
+
         /// <summary>
         ///     Sets a single line border style for this <see cref="ContextMenuStrip"/>.
         /// </summary>
@@ -36,6 +162,8 @@ namespace SilDev.Forms
         /// </param>
         public static void SetFixedSingle(this ContextMenuStrip contextMenuStrip, Color? borderColor = null)
         {
+            if (contextMenuStrip == null)
+                return;
             contextMenuStrip.Paint += (sender, args) =>
             {
                 try
