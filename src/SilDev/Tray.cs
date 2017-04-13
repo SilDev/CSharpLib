6882db30e94f18ee5f3dc71af06bfa49618d2329
Si13n7 <si13n7@live.de>
2017-04-13 18:16:11 +0200

    Make Tray.RefreshVisibleArea working again (not really fixed yet).

--- src/SilDev/Tray.cs
+++ src/SilDev/Tray.cs
@@ -5,7 +5,7 @@
 // ==============================================
 // 
 // Filename: Tray.cs
-// Version:  2017-04-12 17:16
+// Version:  2017-04-13 18:08
 // 
 // Copyright (c) 2017, Si13n7 Developments (r)
 // All rights reserved.
@@ -19,6 +19,8 @@ namespace SilDev
     using System.ComponentModel;
     using System.Drawing;
     using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Windows.Forms;
 
     /// <summary>
     ///     Provides the functionality to manage the items of the system tray area
@@ -29,8 +31,7 @@ namespace SilDev
         private static readonly object Locker = new object();
 
         /// <summary>
-        ///     Refreshes the visible system tray area of the taskbar (experimental
-        ///     function).
+        ///     Refreshes the visible system tray area of the taskbar.
         /// </summary>
         public static bool RefreshVisibleArea()
         {
@@ -52,9 +53,47 @@ namespace SilDev
                 }
                 Rectangle rect;
                 WinApi.UnsafeNativeMethods.GetClientRect(hWnd, out rect);
+
+                /* This function cause a critical issue that
+                 * deletes the sort order in some cases...
+
                 for (var x = 0; x < rect.Right; x += 5)
                     for (var y = 0; y < rect.Bottom; y += 5)
-                        WinApi.UnsafeNativeMethods.SendMessage(hWnd, (uint)WinApi.WindowMenuFunc.WM_MOUSEMOVE, IntPtr.Zero, new IntPtr((y << 16) + x));
+                        if ((uint)WinApi.UnsafeNativeMethods.SendMessage(hWnd, (uint)WinApi.WindowMenuFunc.WM_MOUSEMOVE, IntPtr.Zero, new IntPtr((y << 16) + x)) != 1)
+                            WinApi.ThrowLastError();
+                */
+
+                // Ugly fix...
+                var screen = Screen.PrimaryScreen.Bounds;
+                switch (TaskBar.GetLocation())
+                {
+                    case TaskBar.Location.Left:
+                        rect.Height += 112;
+                        rect.Location = new Point(screen.Left, screen.Bottom - rect.Bottom);
+                        break;
+                    case TaskBar.Location.Top:
+                        rect.Width += 112;
+                        rect.Location = new Point(screen.Right - rect.Right, screen.Top);
+                        break;
+                    case TaskBar.Location.Right:
+                        rect.Height += 112;
+                        rect.Location = new Point(screen.Right - rect.Right, screen.Bottom - rect.Bottom);
+                        break;
+                    case TaskBar.Location.Bottom:
+                        rect.Width += 112;
+                        rect.Location = new Point(screen.Right - rect.Right, screen.Bottom - rect.Bottom);
+                        break;
+                    default:
+                        throw new WarningException("The taskbar is hidden.");
+                }
+                var position = Cursor.Position;
+                for (var x = 0; x < rect.Width; x += 10)
+                    for (var y = 0; y < rect.Height; y += 10)
+                    {
+                        Cursor.Position = new Point(rect.X + x, rect.Y + y);
+                        Thread.Sleep(1);
+                    }
+                Cursor.Position = position;
                 return true;
             }
             catch (Exception ex)
@@ -70,102 +109,86 @@ namespace SilDev
         /// </summary>
         public static void RemovePhantomIcons()
         {
-            try
+            var tBarBtn64 = new ToolBarButton64();
+            var tBarBtn32 = new ToolBarButton32();
+            var trayData = new TrayData();
+            lock (Locker)
             {
-                var tBarBtn64 = new ToolBarButton64();
-                var tBarBtn32 = new ToolBarButton32();
-                var trayData = new TrayData();
-                var itemFound = false;
-                var totalRemovedCount = 0;
-                var totalItemCount = 0;
-                lock (Locker)
-                {
-                    for (var pass = 1; pass < 3; pass++)
+                for (var pass = 1; pass < 3; pass++)
+                    for (var kind = 0; kind < 2; kind++)
                     {
-                        for (var kind = 0; kind < 2; kind++)
+                        var hWnd = IntPtr.Zero;
+                        string[] names;
+                        if (kind == 0)
+                            names = new[]
+                            {
+                                "Shell_TrayWnd",
+                                "TrayNotifyWnd",
+                                "SysPager",
+                                "ToolbarWindow32"
+                            };
+                        else
+                            names = new[]
+                            {
+                                "NotifyIconOverflowWindow",
+                                "ToolbarWindow32"
+                            };
+                        try
                         {
-                            var hWnd = IntPtr.Zero;
-                            string[] names;
+                            foreach (var name in names)
+                                WinApi.FindNestedWindow(ref hWnd, name);
+                        }
+                        catch
+                        {
                             if (kind == 0)
-                                names = new[]
+                                continue;
+                            break;
+                        }
+                        using (var pm = new ProcessMemory(hWnd))
+                        {
+                            var rBtnPtr = Environment.Is64BitOperatingSystem ? pm.Allocate(tBarBtn64) : pm.Allocate(tBarBtn32);
+                            pm.Allocate(trayData);
+                            var itemCount = (uint)WinApi.UnsafeNativeMethods.SendMessage(hWnd, 0x418, IntPtr.Zero, IntPtr.Zero);
+                            uint removedCount = 0;
+                            for (var i = 0; i < itemCount; i++)
+                                try
                                 {
-                                    "Shell_TrayWnd",
-                                    "TrayNotifyWnd",
-                                    "SysPager",
-                                    "ToolbarWindow32"
-                                };
-                            else
-                                names = new[]
-                                {
-                                    "NotifyIconOverflowWindow",
-                                    "ToolbarWindow32"
-                                };
-                            try
-                            {
-                                foreach (var name in names)
-                                    WinApi.FindNestedWindow(ref hWnd, name);
-                            }
-                            catch
-                            {
-                                if (kind == 0)
-                                    continue;
-                                break;
-                            }
-                            using (var p = new ProcessMemory(hWnd))
-                            {
-                                var remoteButtonPtr = Environment.Is64BitOperatingSystem ? p.Allocate(tBarBtn64) : p.Allocate(tBarBtn32);
-                                p.Allocate(trayData);
-                                var itemCount = (uint)WinApi.UnsafeNativeMethods.SendMessage(hWnd, 0x418, IntPtr.Zero, IntPtr.Zero);
-                                uint removedCount = 0;
-                                for (var i = 0; i < itemCount; i++)
-                                {
-                                    totalItemCount++;
-                                    var item2 = i - removedCount;
-                                    if (WinApi.UnsafeNativeMethods.SendMessage(hWnd, 0x417, new IntPtr(item2), remoteButtonPtr) != new IntPtr(1))
-                                        throw new ApplicationException("TB_GETBUTTON failed");
+                                    var item = i - removedCount;
+                                    if ((uint)WinApi.UnsafeNativeMethods.SendMessage(hWnd, 0x417, new IntPtr(item), rBtnPtr) != 1)
+                                        throw new ApplicationException("Can not get the button of item '" + item + "' (kind: '" + kind + "').");
                                     if (Environment.Is64BitOperatingSystem)
                                     {
-                                        p.Read(tBarBtn64, remoteButtonPtr);
-                                        p.Read(trayData, tBarBtn64.dwData);
+                                        pm.Read(tBarBtn64, rBtnPtr);
+                                        pm.Read(trayData, tBarBtn64.dwData);
                                     }
                                     else
                                     {
-                                        p.Read(tBarBtn32, remoteButtonPtr);
-                                        p.Read(trayData, tBarBtn32.dwData);
+                                        pm.Read(tBarBtn32, rBtnPtr);
+                                        pm.Read(trayData, tBarBtn32.dwData);
                                     }
                                     var hWnd2 = trayData.hWnd;
                                     if (hWnd2 == IntPtr.Zero)
                                         throw new ApplicationException("Invalid window handle.");
-                                    using (var p2 = new ProcessMemory(hWnd2))
+                                    using (var pm2 = new ProcessMemory(hWnd2))
                                     {
-                                        var name = p2.GetImageFileName();
+                                        var name = pm2.GetImageFileName();
                                         if (!string.IsNullOrWhiteSpace(name))
-                                            Log.Write(nameof(Tray) + " found: '" + name + "' (kind: '" + kind + "').");
-                                        if (pass == 1)
-                                            if (name?.EndsWithEx(".exe") == true)
-                                            {
-                                                itemFound = true;
-                                                break;
-                                            }
+                                            Log.Write("Tray icon found: '" + name + "' (kind: '" + kind + "').");
                                         if (pass < 2 || !string.IsNullOrWhiteSpace(name))
                                             continue;
-                                        if ((uint)WinApi.UnsafeNativeMethods.SendMessage(hWnd, 0x416, new IntPtr(item2), IntPtr.Zero) != 1)
-                                            throw new WarningException(nameof(Tray) + " can not remove the button of '" + name + "' (kind: '" + kind + "').");
+                                        if ((uint)WinApi.UnsafeNativeMethods.SendMessage(hWnd, 0x416, new IntPtr(item), IntPtr.Zero) != 1)
+                                            throw new ApplicationException("Can not remove the button of '" + name + "' (kind: '" + kind + "').");
+                                        Log.Write("Tray icon removed: '" + name + "' (kind: '" + kind + "').");
+                                        itemCount--;
                                         removedCount++;
-                                        totalRemovedCount++;
                                     }
                                 }
-                            }
+                                catch (Exception ex)
+                                {
+                                    Log.Write(ex);
+                                }
                         }
-                        if (totalItemCount != 0 && !itemFound)
-                            throw new WarningException(nameof(Tray) + " failed to find any real icon.");
                     }
-                }
-                throw new WarningException(nameof(Tray) + " removed " + totalRemovedCount + "/" + totalItemCount + " icons.");
-            }
-            catch (Exception ex)
-            {
-                Log.Write(ex);
             }
         }
 
