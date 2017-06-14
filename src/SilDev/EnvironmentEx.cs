6bf56b66dd03f97e307b5815738bbeede514e8f8
Si13n7 <si13n7@live.de>
2017-06-14 11:25:03 +0200

    Improve functions to handle system restore points.

--- src/SilDev/EnvironmentEx.cs
+++ src/SilDev/EnvironmentEx.cs
@@ -5,7 +5,7 @@
 // ==============================================
 // 
 // Filename: EnvironmentEx.cs
-// Version:  2017-06-14 11:16
+// Version:  2017-06-14 11:20
 // 
 // Copyright (c) 2017, Si13n7 Developments (r)
 // All rights reserved.
@@ -483,107 +483,113 @@ namespace SilDev
         }
 
         /// <summary>
-        ///     Determines whether the system restoring is enabled.
+        ///     Provides functionality to handle system restore points.
         /// </summary>
-        public static bool SystemRestoringIsEnabled =>
-            Reg.Read(Registry.LocalMachine, @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore", "RPSessionInterval", 0) > 0;
-
-        /// <summary>
-        ///     The type of event. For more information, see <see cref="CreateSystemRestorePoint"/>.
-        /// </summary>
-        public enum EventType
+        public static class SystemRestore
         {
             /// <summary>
-            ///     A system change has begun. A subsequent nested call does not create a new restore
-            ///     point.
-            ///     <para>
-            ///         Subsequent calls must use <see cref="EventType.EndNestedSystemChange"/>, not
-            ///         <see cref="EventType.EndSystemChange"/>.
-            ///     </para>
+            ///     Determines whether the system restoring is enabled.
             /// </summary>
-            BeginNestedSystemChange = 0x66,
+            public static bool IsEnabled =>
+                Reg.Read(Registry.LocalMachine, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SystemRestore", "RPSessionInterval", 0) > 0;
 
             /// <summary>
-            ///     A system change has begun.
+            ///     The type of event. For more information, see <see cref="Create"/>.
             /// </summary>
-            BeginSystemChange = 0x64,
-
-            /// <summary>
-            ///     A system change has ended.
-            /// </summary>
-            EndNestedSystemChange = 0x67,
-
-            /// <summary>
-            ///     A system change has ended.
-            /// </summary>
-            EndSystemChange = 0x65
-        }
-
-        /// <summary>
-        ///     The type of restore point. For more information, see <see cref="CreateSystemRestorePoint"/>.
-        /// </summary>
-        public enum RestorePointType
-        {
-            /// <summary>
-            ///     An application has been installed.
-            /// </summary>
-            ApplicationInstall = 0x0,
-
-            /// <summary>
-            ///     An application has been uninstalled.
-            /// </summary>
-            ApplicationUninstall = 0x1,
-
-            /// <summary>
-            ///     An application needs to delete the restore point it created. For example, an
-            ///     application would use this flag when a user cancels an installation.
-            /// </summary>
-            CancelledOperation = 0xd,
-
-            /// <summary>
-            ///     A device driver has been installed.
-            /// </summary>
-            DeviceDriverInstall = 0xa,
-
-            /// <summary>
-            ///     An application has had features added or removed.
-            /// </summary>
-            ModifySettings = 0xc
-        }
-
-        /// <summary>
-        ///     Creates a restore point on the local system.
-        /// </summary>
-        /// <param name="description">
-        ///     The description to be displayed so the user can easily identify a restore point.
-        /// </param>
-        /// <param name="eventType">
-        ///     The type of event.
-        /// </param>
-        /// <param name="restorePointType">
-        ///     The type of restore point.
-        /// </param>
-        public static void CreateSystemRestorePoint(string description, EventType eventType, RestorePointType restorePointType)
-        {
-            try
+            public enum EventType
             {
-                if (!SystemRestoringIsEnabled)
-                    throw new WarningException("System restoring is disabled.");
-                var mScope = new ManagementScope("\\\\localhost\\root\\default");
-                var mPath = new ManagementPath("SystemRestore");
-                var options = new ObjectGetOptions();
-                using (var mClass = new ManagementClass(mScope, mPath, options))
-                using (var parameters = mClass.GetMethodParameters("CreateRestorePoint"))
-                {
-                    parameters["Description"] = description;
-                    parameters["EventType"] = (int)eventType;
-                    parameters["RestorePointType"] = (int)restorePointType;
-                    mClass.InvokeMethod("CreateRestorePoint", parameters, null);
-                }
+                /// <summary>
+                ///     A system change has begun. A subsequent nested call does not create a new restore
+                ///     point.
+                ///     <para>
+                ///         Subsequent calls must use <see cref="EndNestedSystemChange"/>, not
+                ///         <see cref="EndSystemChange"/>.
+                ///     </para>
+                /// </summary>
+                BeginNestedSystemChange = 0x66,
+
+                /// <summary>
+                ///     A system change has begun.
+                /// </summary>
+                BeginSystemChange = 0x64,
+
+                /// <summary>
+                ///     A system change has ended.
+                /// </summary>
+                EndNestedSystemChange = 0x67,
+
+                /// <summary>
+                ///     A system change has ended.
+                /// </summary>
+                EndSystemChange = 0x65
             }
-            catch (Exception ex)
+
+            /// <summary>
+            ///     The type of restore point. For more information, see <see cref="Create"/>.
+            /// </summary>
+            public enum PointType
             {
-                Log.Write(ex);
+                /// <summary>
+                ///     An application has been installed.
+                /// </summary>
+                ApplicationInstall = 0x0,
+
+                /// <summary>
+                ///     An application has been uninstalled.
+                /// </summary>
+                ApplicationUninstall = 0x1,
+
+                /// <summary>
+                ///     An application needs to delete the restore point it created. For example, an
+                ///     application would use this flag when a user cancels an installation.
+                /// </summary>
+                CancelledOperation = 0xd,
+
+                /// <summary>
+                ///     A device driver has been installed.
+                /// </summary>
+                DeviceDriverInstall = 0xa,
+
+                /// <summary>
+                ///     An application has had features added or removed.
+                /// </summary>
+                ModifySettings = 0xc
+            }
+
+            /// <summary>
+            ///     Creates a restore point on the local system.
+            /// </summary>
+            /// <param name="description">
+            ///     The description to be displayed so the user can easily identify a restore point.
+            /// </param>
+            /// <param name="eventType">
+            ///     The type of event.
+            /// </param>
+            /// <param name="restorePointType">
+            ///     The type of restore point.
+            /// </param>
+            public static void Create(string description, EventType eventType, PointType restorePointType)
+            {
+                try
+                {
+                    if (!IsEnabled)
+                        throw new WarningException("System restoring is disabled.");
+                    var mScope = new ManagementScope("\\\\localhost\\root\\default");
+                    var mPath = new ManagementPath("SystemRestore");
+                    var options = new ObjectGetOptions();
+                    using (var mClass = new ManagementClass(mScope, mPath, options))
+                        using (var parameters = mClass.GetMethodParameters("CreateRestorePoint"))
+                        {
+                            parameters["Description"] = description;
+                            parameters["EventType"] = (int)eventType;
+                            parameters["RestorePointType"] = (int)restorePointType;
+                            mClass.InvokeMethod("CreateRestorePoint", parameters, null);
+                        }
+                }
+                catch (Exception ex)
+                {
+                    Log.Write(ex);
+                }
             }
         }
     }
