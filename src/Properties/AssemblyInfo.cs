640ab3a8dd7ce512b9dbb41839653439202cebde
Si13n7 <si13n7@live.de>
2017-06-14 11:25:29 +0200

    Update binaries.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("901502cc-aa7d-444e-944a-7fc063c34917")]
 
-[assembly: AssemblyVersion("17.6.7.*")]
+[assembly: AssemblyVersion("17.6.14.*")]
 
 [assembly: NeutralResourcesLanguage("")]
 
