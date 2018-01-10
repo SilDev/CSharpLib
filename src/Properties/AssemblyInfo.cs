e45c921c81ef08a31c5d733ba2cea8dfbdbb238d
Si13n7 <si13n7@live.de>
2018-01-10 07:34:20 +0100

    Update binaries.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("901502cc-aa7d-444e-944a-7fc063c34917")]
 
-[assembly: AssemblyVersion("18.1.8.*")]
+[assembly: AssemblyVersion("18.1.10.*")]
 
 [assembly: NeutralResourcesLanguage("")]
 
