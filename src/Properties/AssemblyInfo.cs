d7fd4bb99675b3b19011aa05fc8b53ef5bd802e3
Si13n7 <admin@si13n7.com>
2019-12-24 09:17:31 +0100

    Update binaries.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("901502cc-aa7d-444e-944a-7fc063c34917")]
 
-[assembly: AssemblyVersion("19.12.21.0")]
+[assembly: AssemblyVersion("19.12.24.0")]
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
