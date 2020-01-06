e1d160153f446cce434d0f1b60f3bba2b9d0e336
Si13n7 <admin@si13n7.com>
2020-01-06 06:46:03 +0100

    Add methods to get AssemblyInfo information from specified assemblies.

--- src/SilDev/AssemblyInfo.cs
+++ src/SilDev/AssemblyInfo.cs
@@ -5,9 +5,9 @@
 // ==============================================
 // 
 // Filename: AssemblyInfo.cs
-// Version:  2019-12-15 14:56
+// Version:  2020-01-06 06:45
 // 
-// Copyright (c) 2019, Si13n7 Developments (r)
+// Copyright (c) 2020, Si13n7 Developments (r)
 // All rights reserved.
 // ______________________________________________
 
@@ -24,62 +24,148 @@ namespace SilDev
     public static class AssemblyInfo
     {
         /// <summary>
-        ///     Gets company name information.
+        ///     Gets company name information of the current process.
         /// </summary>
-        public static string Company => GetAssembly<AssemblyCompanyAttribute>()?.Company;
+        public static string Company =>
+            GetCompanyFrom(Assembly.GetEntryAssembly());
 
         /// <summary>
-        ///     Gets configuration information.
+        ///     Gets configuration information of the current process.
         /// </summary>
-        public static string Configuration => GetAssembly<AssemblyDescriptionAttribute>()?.Description;
+        public static string Configuration =>
+            GetConfigurationFrom(Assembly.GetEntryAssembly());
 
         /// <summary>
-        ///     Gets copyright information.
+        ///     Gets copyright information of the current process.
         /// </summary>
-        public static string Copyright => GetAssembly<AssemblyCopyrightAttribute>()?.Copyright;
+        public static string Copyright =>
+            GetCopyrightFrom(Assembly.GetEntryAssembly());
 
         /// <summary>
-        ///     Gets description information.
+        ///     Gets description information of the current process.
         /// </summary>
-        public static string Description => GetAssembly<AssemblyDescriptionAttribute>()?.Description;
+        public static string Description =>
+            GetDescriptionFrom(Assembly.GetEntryAssembly());
 
         /// <summary>
-        ///     Gets file version information.
+        ///     Gets file version information of the current process.
         /// </summary>
-        public static string FileVersion => GetAssembly<AssemblyFileVersionAttribute>()?.Version;
+        public static string FileVersion =>
+            GetFileVersionFrom(Assembly.GetEntryAssembly());
 
         /// <summary>
-        ///     Gets product information.
+        ///     Gets product information of the current process.
         /// </summary>
-        public static string Product => GetAssembly<AssemblyProductAttribute>()?.Product;
+        public static string Product =>
+            GetProductFrom(Assembly.GetEntryAssembly());
 
         /// <summary>
-        ///     Gets title information.
+        ///     Gets title information of the current process.
         /// </summary>
-        public static string Title => GetAssembly<AssemblyTitleAttribute>()?.Title;
+        public static string Title =>
+            GetTitleFrom(Assembly.GetEntryAssembly());
+
+        /// <summary>
+        ///     Gets trademark information of the current process.
+        /// </summary>
+        public static string Trademark =>
+            GetTrademarkFrom(Assembly.GetEntryAssembly());
+
+        /// <summary>
+        ///     Gets version information of the current process.
+        /// </summary>
+        public static string Version =>
+            GetVersionFrom(Assembly.GetEntryAssembly());
+
+        /// <summary>
+        ///     Gets company name information from specified assembly, if available.
+        /// </summary>
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetCompanyFrom(Assembly element) =>
+            GetAssembly<AssemblyCompanyAttribute>(element)?.Company;
+
+        /// <summary>
+        ///     Gets configuration information from specified assembly, if available.
+        /// </summary>
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetConfigurationFrom(Assembly element) =>
+            GetAssembly<AssemblyDescriptionAttribute>(element)?.Description;
+
+        /// <summary>
+        ///     Gets copyright information from specified assembly, if available.
+        /// </summary>
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetCopyrightFrom(Assembly element) =>
+            GetAssembly<AssemblyCopyrightAttribute>(element)?.Copyright;
+
+        /// <summary>
+        ///     Gets description information from specified assembly, if available.
+        /// </summary>
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetDescriptionFrom(Assembly element) =>
+            GetAssembly<AssemblyDescriptionAttribute>(element)?.Description;
+
+        /// <summary>
+        ///     Gets file version information from specified assembly, if available.
+        /// </summary>
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetFileVersionFrom(Assembly element) =>
+            GetAssembly<AssemblyFileVersionAttribute>(element)?.Version;
+
+        /// <summary>
+        ///     Gets product information from specified assembly, if available.
+        /// </summary>
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetProductFrom(Assembly element) =>
+            GetAssembly<AssemblyProductAttribute>(element)?.Product;
+
+        /// <summary>
+        ///     Gets title information from specified assembly, if available.
+        /// </summary>
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetTitleFrom(Assembly element) =>
+            GetAssembly<AssemblyTitleAttribute>(element)?.Title;
 
         /// <summary>
         ///     Gets trademark information.
         /// </summary>
-        public static string Trademark => GetAssembly<AssemblyTrademarkAttribute>()?.Trademark;
+        public static string GetTrademarkFrom(Assembly element) =>
+            GetAssembly<AssemblyTrademarkAttribute>(element)?.Trademark;
 
         /// <summary>
-        ///     Gets version information.
+        ///     Gets version information from specified assembly, if available.
         /// </summary>
-        public static string Version => Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+        /// <param name="element">
+        ///     The assembly element to get the information.
+        /// </param>
+        public static string GetVersionFrom(Assembly element) =>
+            element?.GetName().Version?.ToString();
 
-        private static TSource GetAssembly<TSource>() where TSource : Attribute
+        private static TSource GetAssembly<TSource>(Assembly element) where TSource : Attribute
         {
             try
             {
-                var element = Assembly.GetEntryAssembly();
                 if (element == null)
-                    return default;
-                var assembly = Attribute.GetCustomAttribute(element, typeof(TSource));
-                return (TSource)assembly;
+                    throw new ArgumentNullException(nameof(element));
+                return (TSource)Attribute.GetCustomAttribute(element, typeof(TSource));
             }
             catch (Exception ex) when (ex.IsCaught())
             {
+                Log.Write(ex);
                 return default;
             }
         }
