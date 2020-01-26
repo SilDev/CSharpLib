97d7716943cbf8364c09df20f874480513d66bef
Si13n7 <admin@si13n7.com>
2020-01-26 11:02:33 +0100

    Improve compare function of AlphaNumericComparer class.

--- src/SilDev/AlphaNumericComparer.cs
+++ src/SilDev/AlphaNumericComparer.cs
@@ -5,7 +5,7 @@
 // ==============================================
 // 
 // Filename: AlphaNumericComparer.cs
-// Version:  2020-01-24 20:58
+// Version:  2020-01-26 11:01
 // 
 // Copyright (c) 2020, Si13n7 Developments(tm)
 // All rights reserved.
@@ -18,6 +18,7 @@ namespace SilDev
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Runtime.Serialization;
     using System.Security;
 
@@ -68,25 +69,6 @@ namespace SilDev
         }
 
         /// <summary>
-        ///     Sets the <see cref="SerializationInfo"/> object for this instance.
-        /// </summary>
-        /// <param name="info">
-        ///     The object that holds the serialized object data.
-        /// </param>
-        /// <param name="context">
-        ///     The contextual information about the source or destination.
-        /// </param>
-        [SecurityCritical]
-        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            if (info == null)
-                throw new ArgumentNullException(nameof(info));
-            if (Log.DebugMode > 1)
-                Log.Write($"{nameof(AlphaNumericComparer)}.get({nameof(SerializationInfo)}, {nameof(StreamingContext)}) => info: {Json.Serialize(info)}, context: {Json.Serialize(context)}");
-            info.AddValue(nameof(Descendant), Descendant);
-        }
-
-        /// <summary>
         ///     Compare two specified objects and returns an integer that indicates their
         ///     relative position in the sort order.
         /// </summary>
@@ -110,41 +92,17 @@ namespace SilDev
                 var i2 = 0;
                 while (i1 < s1.Length && i2 < s2.Length)
                 {
-                    var c1 = s1[i1];
-                    var ca1 = new char[s1.Length];
-                    var l1 = 0;
-                    do
-                    {
-                        ca1[l1++] = c1;
-                        i1++;
-                        if (i1 >= s1.Length)
-                            break;
-                        c1 = s1[i1];
-                    }
-                    while (char.IsDigit(c1) == char.IsDigit(ca1[0]));
-                    var c2 = s2[i2];
-                    var ca2 = new char[s2.Length];
-                    var l2 = 0;
-                    do
-                    {
-                        ca2[l2++] = c2;
-                        i2++;
-                        if (i2 >= s2.Length)
-                            break;
-                        c2 = s2[i2];
-                    }
-                    while (char.IsDigit(c2) == char.IsDigit(ca2[0]));
-                    var str1 = new string(ca1);
-                    var str2 = new string(ca2);
+                    var c1 = GetChunk(s1, ref i1);
+                    var c2 = GetChunk(s2, ref i2);
                     int r;
-                    if (char.IsDigit(ca1[0]) && char.IsDigit(ca2[0]))
-                    {
-                        var ch1 = int.Parse(str1, CultureConfig.GlobalCultureInfo);
-                        var ch2 = int.Parse(str2, CultureConfig.GlobalCultureInfo);
-                        r = ch1.CompareTo(ch2);
-                    }
+                    if (!char.IsDigit(c1[0]) || !char.IsDigit(c2[0]))
+                        r = string.Compare(c1, c2, StringComparison.CurrentCulture);
                     else
-                        r = string.Compare(str1, str2, StringComparison.InvariantCulture);
+                    {
+                        var n1 = int.Parse(c1, CultureInfo.CurrentCulture);
+                        var n2 = int.Parse(c2, CultureInfo.CurrentCulture);
+                        r = n1.CompareTo(n2);
+                    }
                     if (r != 0)
                         return r;
                 }
@@ -152,8 +110,27 @@ namespace SilDev
             }
             catch (Exception ex) when (ex.IsCaught())
             {
-                return string.Compare(s1, s2, StringComparison.InvariantCulture);
+                return string.Compare(s1, s2, StringComparison.CurrentCulture);
             }
+        }
+
+        /// <summary>
+        ///     Sets the <see cref="SerializationInfo"/> object for this instance.
+        /// </summary>
+        /// <param name="info">
+        ///     The object that holds the serialized object data.
+        /// </param>
+        /// <param name="context">
+        ///     The contextual information about the source or destination.
+        /// </param>
+        [SecurityCritical]
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException(nameof(info));
+            if (Log.DebugMode > 1)
+                Log.Write($"{nameof(AlphaNumericComparer)}.get({nameof(SerializationInfo)}, {nameof(StreamingContext)}) => info: {Json.Serialize(info)}, context: {Json.Serialize(context)}");
+            info.AddValue(nameof(Descendant), Descendant);
         }
 
         /// <summary>
@@ -164,5 +141,15 @@ namespace SilDev
         /// </param>
         protected virtual string GetString(object value) =>
             value as string;
+
+        private static string GetChunk(string str, ref int i)
+        {
+            var pos = 0;
+            var len = str.Length;
+            var ca = new char[len];
+            do ca[pos++] = str[i];
+            while (++i < len && char.IsDigit(str[i]) == char.IsDigit(ca[0]));
+            return new string(ca);
+        }
     }
 }
