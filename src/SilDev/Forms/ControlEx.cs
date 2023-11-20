96e099456fef2e063d237a173683bd66d9b86f16
Si13n7 <admin@si13n7.com>
2023-11-20 23:31:10 +0100

    Add method to change almost all nested control colors.

--- src/SilDev/Forms/ControlEx.cs
+++ src/SilDev/Forms/ControlEx.cs
@@ -5,9 +5,9 @@
 // ==============================================
 // 
 // Filename: ControlEx.cs
-// Version:  2021-04-22 19:45
+// Version:  2023-11-20 23:26
 // 
-// Copyright (c) 2021, Si13n7 Developments(tm)
+// Copyright (c) 2023, Si13n7 Developments(tm)
 // All rights reserved.
 // ______________________________________________
 
@@ -23,6 +23,7 @@ namespace SilDev.Forms
     using System.Windows.Forms;
     using Drawing;
     using Properties;
+    using static System.Windows.Forms.ListViewItem;
 
     /// <summary>
     ///     Specifies the border style for a control.
@@ -46,6 +47,52 @@ namespace SilDev.Forms
     }
 
     /// <summary>
+    ///     Specifies the color mode to apply for a control.
+    /// </summary>
+    public enum ControlExColorMode
+    {
+        /// <summary>
+        ///     Increases the brightness.
+        /// </summary>
+        Light = 0,
+
+        /// <summary>
+        ///     Decreases the brightness.
+        /// </summary>
+        Dark = 1,
+
+        /// <summary>
+        ///     Increases the brightness a little more.
+        /// </summary>
+        LightLight = 2,
+
+        /// <summary>
+        ///     Decreases the brightness a little more.
+        /// </summary>
+        DarkDark = 3,
+
+        /// <summary>
+        ///     Extremely increases brightness.
+        /// </summary>
+        LightLightLight = 4,
+
+        /// <summary>
+        ///     Extremely decreases brightness.
+        /// </summary>
+        DarkDarkDark = 5,
+
+        /// <summary>
+        ///     Inverts the three RGB component (red, green, blue) values.
+        /// </summary>
+        InvertRgb = 6,
+
+        /// <summary>
+        ///     Inherit from a second <see cref="ControlExColorMode"/> option.
+        /// </summary>
+        Inherit = 7,
+    }
+
+    /// <summary>
     ///     Expands the functionality for the <see cref="Control"/> class.
     /// </summary>
     public static class ControlEx
@@ -58,7 +105,7 @@ namespace SilDev.Forms
         /// </param>
         public static Control GetAncestor(this Control control)
         {
-            if (!(control is { } c))
+            if (control is not { } c)
                 return default;
             var cur = c;
             while (cur.Parent != null)
@@ -86,6 +133,116 @@ namespace SilDev.Forms
             {
                 Log.Write(ex);
                 return false;
+            }
+        }
+
+        /// <summary>
+        ///     Changes the three RGB component values (Red, Green, Blue) of the
+        ///     <see cref="Control.ForeColor"/> and <see cref="Control.BackColor"/>
+        ///     properties inside all nested controls of the specified control.
+        ///     <para>
+        ///         Please note that it is not enough to change the colors of all control
+        ///         elements to achieve a full color mode. To do this you would have to
+        ///         create inherited classes that override important methods, or handle
+        ///         DrawHandlerEvents in which you would have to handle every little thing
+        ///         so that it doesn't look crappy..
+        ///     </para>
+        /// </summary>
+        /// <param name="control">
+        ///     The control to change all colors.
+        /// </param>
+        /// <param name="foreMode">
+        ///     The <see cref="Control.ForeColor"/> mode to apply.
+        /// </param>
+        /// <param name="backMode">
+        ///     The <see cref="Control.BackColor"/> mode to apply.
+        /// </param>
+        public static void ChangeColorMode(this Control control, ControlExColorMode backMode = ControlExColorMode.DarkDark, ControlExColorMode foreMode = ControlExColorMode.Inherit)
+        {
+            if (control == default)
+                return;
+            if (backMode == ControlExColorMode.Inherit)
+                backMode = Inherit(foreMode);
+            if (foreMode == ControlExColorMode.Inherit)
+                foreMode = Inherit(backMode);
+            control.SuspendLayout();
+            var queue = new Queue<Control>();
+            queue.Enqueue(control);
+            do
+            {
+                var parent = queue.Dequeue();
+                parent.ForeColor = GetColor(parent.ForeColor, foreMode);
+                parent.BackColor = GetColor(parent.BackColor, backMode);
+                switch (parent)
+                {
+                    case ContextMenuStrip cms:
+                        foreach (var item in cms.Items.Cast<ToolStripItem>())
+                        {
+                            item.ForeColor = GetColor(item.ForeColor, foreMode);
+                            item.BackColor = GetColor(item.BackColor, backMode);
+                        }
+                        break;
+                    case Button { FlatStyle: FlatStyle.Flat } button:
+                        button.FlatAppearance.BorderColor = GetColor(button.FlatAppearance.BorderColor, foreMode);
+                        button.FlatAppearance.CheckedBackColor = GetColor(button.FlatAppearance.CheckedBackColor, backMode);
+                        button.FlatAppearance.MouseDownBackColor = GetColor(button.FlatAppearance.MouseDownBackColor, backMode);
+                        button.FlatAppearance.MouseDownBackColor = GetColor(button.FlatAppearance.MouseDownBackColor, backMode);
+                        break;
+                    case ListView listView:
+                        foreach (var item in listView.Items.Cast<ListViewItem>())
+                        {
+                            item.ForeColor = GetColor(item.ForeColor, foreMode);
+                            item.BackColor = GetColor(item.BackColor, backMode);
+                            foreach (var subItem in item.SubItems.Cast<ListViewSubItem>())
+                            {
+                                subItem.ForeColor = GetColor(subItem.ForeColor, foreMode);
+                                subItem.BackColor = GetColor(subItem.BackColor, backMode);
+                            }
+                        }
+                        break;
+                    case RichTextBox richTextBox:
+                        richTextBox.SelectionColor = GetColor(richTextBox.SelectionColor, foreMode);
+                        richTextBox.SelectionBackColor = GetColor(richTextBox.SelectionBackColor, backMode);
+                        break;
+                    case TabControl tabControl:
+                        foreach (var tabPage in tabControl.TabPages.Cast<Control>())
+                            queue.Enqueue(tabPage);
+                        break;
+                }
+                if (parent.ContextMenuStrip is Control c)
+                    queue.Enqueue(c);
+                foreach (var child in parent.Controls.Cast<Control>())
+                    queue.Enqueue(child);
+            }
+            while (queue.Any());
+            control.ResumeLayout(true);
+
+            static ControlExColorMode Inherit(ControlExColorMode mode) =>
+                mode switch
+                {
+                    ControlExColorMode.Light => ControlExColorMode.Dark,
+                    ControlExColorMode.Dark => ControlExColorMode.Light,
+                    ControlExColorMode.LightLight => ControlExColorMode.DarkDark,
+                    ControlExColorMode.DarkDark => ControlExColorMode.DarkDark,
+                    ControlExColorMode.LightLightLight => ControlExColorMode.DarkDarkDark,
+                    ControlExColorMode.DarkDarkDark => ControlExColorMode.LightLightLight,
+                    _ => ControlExColorMode.InvertRgb
+                };
+
+            static Color GetColor(Color color, ControlExColorMode mode)
+            {
+                if (color == Color.Empty || color == Color.Transparent)
+                    return color;
+                return mode switch
+                {
+                    ControlExColorMode.Light => color.EnsureLight(),
+                    ControlExColorMode.Dark => color.EnsureDark(),
+                    ControlExColorMode.LightLight => color.EnsureLightLight(),
+                    ControlExColorMode.DarkDark => color.EnsureDarkDark(),
+                    ControlExColorMode.LightLightLight => color.EnsureLightLightLight(),
+                    ControlExColorMode.DarkDarkDark => color.EnsureDarkDarkDark(),
+                    _ => color.InvertRgb()
+                };
             }
         }
 
