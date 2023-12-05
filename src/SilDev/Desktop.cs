bda45476ed81e8bf09b9abaa0d97b08d31bd59c3
Si13n7 <admin@si13n7.com>
2023-12-05 13:52:54 +0100

    Add method to round corners of a window.

--- src/SilDev/Desktop.cs
+++ src/SilDev/Desktop.cs
@@ -5,7 +5,7 @@
 // ==============================================
 // 
 // Filename: Desktop.cs
-// Version:  2023-12-05 13:10
+// Version:  2023-12-05 13:51
 // 
 // Copyright (c) 2023, Si13n7 Developments(tm)
 // All rights reserved.
@@ -19,6 +19,7 @@ namespace SilDev
     using System.Drawing;
     using System.IO;
     using SHDocVw;
+    using static WinApi;
 
     /// <summary>
     ///     Provides the functionality to manage desktop functions.
@@ -57,8 +58,8 @@ namespace SilDev
         {
             if (hWnd == IntPtr.Zero || !EnvironmentEx.IsAtLeastWindows(10, 17763))
                 return;
-            _ = WinApi.NativeHelper.SetWindowTheme(hWnd, "DarkMode_Explorer");
-            WinApi.NativeHelper.DwmSetWindowAttribute(hWnd, WinApi.DwmWindowAttribute.DwmwaUseImmersiveDarkMode);
+            _ = NativeHelper.SetWindowTheme(hWnd, "DarkMode_Explorer");
+            NativeHelper.DwmSetWindowAttribute(hWnd, DwmWindowAttribute.DwmwaUseImmersiveDarkMode);
         }
 
         /// <summary>
@@ -78,7 +79,27 @@ namespace SilDev
         {
             if (hWnd == IntPtr.Zero || !EnvironmentEx.IsAtLeastWindows(11))
                 return;
-            WinApi.NativeHelper.DwmSetWindowAttribute(hWnd, WinApi.DwmWindowAttribute.DwmwaMicaEffect);
+            NativeHelper.DwmSetWindowAttribute(hWnd, DwmWindowAttribute.DwmwaMicaEffect);
+        }
+
+        /// <summary>
+        ///     Rounds the corners of the window under the specified handle, if
+        ///     appropriate.
+        ///     <para>
+        ///         &#9888; This feature requires at least Windows 11.
+        ///     </para>
+        /// </summary>
+        /// <param name="hWnd">
+        ///     Handle to a window.
+        /// </param>
+        /// <param name="small">
+        ///     Enables a smaller radius.
+        /// </param>
+        public static void RoundCorners(IntPtr hWnd, bool small = false)
+        {
+            if (hWnd == IntPtr.Zero || !EnvironmentEx.IsAtLeastWindows(11))
+                return;
+            NativeHelper.DwmSetWindowAttribute(hWnd, small ? DwmWindowCornerPreference.DwmwCpRoundSmall : DwmWindowCornerPreference.DwmwCpRound);
         }
 
         /// <summary>
@@ -93,7 +114,7 @@ namespace SilDev
         /// </param>
         public static float GetDpi(IntPtr hWnd = default)
         {
-            var handle = hWnd == default ? WinApi.NativeMethods.GetDesktopWindow() : hWnd;
+            var handle = hWnd == default ? NativeMethods.GetDesktopWindow() : hWnd;
             using var graphics = Graphics.FromHwnd(handle);
             return Math.Max(graphics.DpiX, graphics.DpiY);
         }
@@ -111,14 +132,14 @@ namespace SilDev
         /// </param>
         public static void Refresh(bool explorer = true, bool extended = false)
         {
-            var hWnd = WinApi.NativeHelper.FindWindow("Progman", "Program Manager");
+            var hWnd = NativeHelper.FindWindow("Progman", "Program Manager");
             var cNames = new[]
             {
                 "SHELLDLL_DefView",
                 "SysListView32"
             };
             foreach (var cName in cNames)
-                WinApi.NativeHelper.FindNestedWindow(ref hWnd, cName);
+                NativeHelper.FindNestedWindow(ref hWnd, cName);
             InputDevice.SendKeyState(hWnd, VirtualKey.F5, VirtualKeyState.KeyDown);
             InputDevice.SendKeyState(hWnd, VirtualKey.F5, VirtualKeyState.KeyUp);
             if (explorer)
