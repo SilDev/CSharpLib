3ef55e2006667e3392ebdcfe00ad879857f37b90
Si13n7 <admin@si13n7.com>
2023-12-13 00:06:38 +0100

    Update binaries.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("901502cc-aa7d-444e-944a-7fc063c34917")]
 
-[assembly: AssemblyVersion("23.12.12.0")]
+[assembly: AssemblyVersion("23.12.13.0")]
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
