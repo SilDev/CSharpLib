61a210547b99b00d1fff5b537dfaa84f2c477b52
Si13n7 <admin@si13n7.com>
2023-12-16 17:21:29 +0100

    Add different methods to retrieve CultureInfo data.

--- src/SilDev/CultureConfig.cs
+++ src/SilDev/CultureConfig.cs
@@ -5,9 +5,9 @@
 // ==============================================
 // 
 // Filename: CultureConfig.cs
-// Version:  2021-04-22 19:46
+// Version:  2023-12-16 17:19
 // 
-// Copyright (c) 2021, Si13n7 Developments(tm)
+// Copyright (c) 2023, Si13n7 Developments(tm)
 // All rights reserved.
 // ______________________________________________
 
@@ -17,6 +17,7 @@ namespace SilDev
 {
     using System;
     using System.Globalization;
+    using System.Linq;
     using System.Threading;
 
     /// <summary>
@@ -59,20 +60,14 @@ namespace SilDev
             set
             {
                 lock (SyncObject)
-                    switch (value)
+                    _globalStringComparison = value switch
                     {
-                        case StringComparison.InvariantCulture:
-                        case StringComparison.InvariantCultureIgnoreCase:
-                            _globalStringComparison = StringComparison.InvariantCulture;
-                            break;
-                        case StringComparison.Ordinal:
-                        case StringComparison.OrdinalIgnoreCase:
-                            _globalStringComparison = StringComparison.Ordinal;
-                            break;
-                        default:
-                            _globalStringComparison = StringComparison.CurrentCulture;
-                            break;
-                    }
+                        StringComparison.InvariantCulture => StringComparison.InvariantCulture,
+                        StringComparison.InvariantCultureIgnoreCase => StringComparison.InvariantCulture,
+                        StringComparison.Ordinal => StringComparison.Ordinal,
+                        StringComparison.OrdinalIgnoreCase => StringComparison.Ordinal,
+                        _ => StringComparison.CurrentCulture
+                    };
             }
         }
 
@@ -89,20 +84,14 @@ namespace SilDev
             set
             {
                 lock (SyncObject)
-                    switch (value)
+                    _globalStringComparisonIgnoreCase = value switch
                     {
-                        case StringComparison.InvariantCulture:
-                        case StringComparison.InvariantCultureIgnoreCase:
-                            _globalStringComparisonIgnoreCase = StringComparison.InvariantCultureIgnoreCase;
-                            break;
-                        case StringComparison.Ordinal:
-                        case StringComparison.OrdinalIgnoreCase:
-                            _globalStringComparisonIgnoreCase = StringComparison.OrdinalIgnoreCase;
-                            break;
-                        default:
-                            _globalStringComparisonIgnoreCase = StringComparison.CurrentCultureIgnoreCase;
-                            break;
-                    }
+                        StringComparison.InvariantCulture => StringComparison.InvariantCultureIgnoreCase,
+                        StringComparison.InvariantCultureIgnoreCase => StringComparison.InvariantCultureIgnoreCase,
+                        StringComparison.Ordinal => StringComparison.OrdinalIgnoreCase,
+                        StringComparison.OrdinalIgnoreCase => StringComparison.OrdinalIgnoreCase,
+                        _ => StringComparison.CurrentCultureIgnoreCase
+                    };
             }
         }
 
@@ -117,5 +106,81 @@ namespace SilDev
                 return _syncObject;
             }
         }
+
+        /// <summary>
+        ///     Gets the <see cref="CultureInfo"/> object of the specified language.
+        /// </summary>
+        /// <param name="englishName">
+        ///     The culture name in English.
+        /// </param>
+        public static CultureInfo GetCultureInfo(string englishName)
+        {
+            if (string.IsNullOrWhiteSpace(englishName))
+                return CultureInfo.CurrentCulture;
+            englishName = englishName switch
+            {
+                "EnglishGB" => "English (United Kingdom)",
+                "PortugueseBR" => "Portuguese (Brazil)",
+                "Portuguese" => "Portuguese (Portugal)",
+                "SerbianLatin" => "Serbian (Latin)",
+                "SpanishInternational" => "Spanish (Spain, International Sort)",
+                "Spanish" => "Spanish (Spain, International Sort)",
+                "Farsi" => "Persian",
+                "SimpChinese" => "Chinese (Simplified, China)",
+                "TradChinese" => "Chinese (Traditional)",
+                _ => englishName
+            };
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            return cultures.FirstOrDefault(ci => ci.EnglishName.EqualsEx(englishName)) ?? CultureInfo.CurrentCulture;
+        }
+
+        /// <summary>
+        ///     Gets the <see cref="CultureInfo"/> object of the specified language
+        ///     identify code.
+        /// </summary>
+        /// <param name="code">
+        ///     A number that identifies the culture.
+        /// </param>
+        public static CultureInfo GetCultureInfo(int code)
+        {
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            return cultures.FirstOrDefault(ci => ci.TextInfo.LCID == code) ?? CultureInfo.CurrentCulture;
+        }
+
+        /// <summary>
+        ///     Gets the identify code of the specified language.
+        /// </summary>
+        /// <param name="englishName">
+        ///     The culture name in English.
+        /// </param>
+        public static int GetCultureId(string englishName) =>
+            GetCultureInfo(englishName).TextInfo.LCID;
+
+        /// <summary>
+        ///     Gets the language name in English of the specified language identify code.
+        /// </summary>
+        /// <param name="code">
+        ///     A number that identifies the culture.
+        /// </param>
+        public static string GetCultureEnglishName(int code) =>
+            GetCultureInfo(code).EnglishName;
+
+        /// <summary>
+        ///     Gets the native language name of the specified language.
+        /// </summary>
+        /// <param name="englishName">
+        ///     The culture name in English.
+        /// </param>
+        public static string GetCultureNativeName(string englishName) =>
+            GetCultureInfo(englishName).NativeName;
+
+        /// <summary>
+        ///     Gets the native language name of the specified language identify code.
+        /// </summary>
+        /// <param name="code">
+        ///     A number that identifies the culture.
+        /// </param>
+        public static string GetCultureNativeName(int code) =>
+            GetCultureInfo(code).NativeName;
     }
 }
